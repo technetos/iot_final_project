6b6c4ccf075a78b98ce4934b8f537950ca2f9f12
technetos <mrgould93@gmail.com>
2018-12-01 00:29:45 -0500

    add credentials

--- rfid_client.py
+++ rfid_client.py
@@ -4,6 +4,10 @@ import aiohttp
 import argparse
 import os
 
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+
 parser = argparse.ArgumentParser(description='RFID CLIENT')
 parser.add_argument('-host', help="url host of the resource server")
 parser.add_argument('-port', help="the port the resource server is running on")
@@ -56,11 +60,49 @@ async def main():
     current_user = None
     url = resource_server + api_base_path
     async with aiohttp.ClientSession() as session:
+        card_data = await read_credentials()
         user_data = await login('rfidpi', 'iotthing', 'df5c9486-2b2b-4cc8-a7bf-3bba73772d2f')
         current_user = LoginResponse(user_data)
         key = await get_file_decryption_key(current_user.access_token)
         print(key)
         did_logout = await logout(current_user.access_token)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+async def read_credentials():
+    continue_reading = True
+
+    # Create an object of the class MFRC522
+    MIFAREReader = MFRC522.MFRC522()
+
+    while continue_reading:
+        # Scan for cards    
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # If a card is found
+        if status == MIFAREReader.MI_OK:
+            print("Card detected")
+
+        # Get the UID of the card
+        (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+            print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+
+            # This is the default key for authentication
+            key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+
+            # Select the scanned tag
+            MIFAREReader.MFRC522_SelectTag(uid)
+
+            # Authenticate
+            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+            # Check if authenticated
+            if status == MIFAREReader.MI_OK:
+                return await MIFAREReader.MFRC522_Read(8)
+                MIFAREReader.MFRC522_StopCrypto1()
+            else:
+                print("Authentication error")
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
